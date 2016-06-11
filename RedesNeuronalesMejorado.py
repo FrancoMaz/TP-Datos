e7c5735f51fae8a00d1813d41dac216795756e63
Franco Mazzoni <franco.mazzoni73@gmail.com>
2016-06-11 14:51:35 -0300

    modificaciones

--- RedesNeuronalesMejorado.py
+++ RedesNeuronalesMejorado.py
@@ -33,7 +33,7 @@ def inicializacionPesos(listaPesos,entrada,salida):
 		listaPesos[i] = []
 		for j in range(0,entrada):
 			listaPesos[i].append(random.random())
-		listaPesos[i].append(UMBRALINICIAL)
+
 	
 #Inicializacion de vector de factores de cambio------------
 def inicializacionFactorDeCambio(factoresDeCambio,cantidadNeuronas):
@@ -50,10 +50,10 @@ def factorDeCambioCapasOcultas(salidaCapaActual,factorDeCambioSiguiente,peso):
 	
 def backpropagation(salidasDeseadas,salidasCapaFinal,salidasCapaOculta1,salidasCapaOculta2,wPesoCapa1,wPesoCapa2,wPesoCapaFinal,entradasCapaInicial):
 	for i in range(0,NUM_CLASES):
-		factoresDeCambio[CANTIDADCAPAS-1][i] = factorDeCambioCapaFinal(salidasDeseadas[i],salidasCapaFinal[i])
+		factoresDeCambio[CANTIDADCAPAS-2][i] = float(factorDeCambioCapaFinal(salidasDeseadas[i],salidasCapaFinal[i]))
 		for j in range(0,NEURONASCAPAOCULTA2):
-			wPesoCapaFinal[i][j] = descensoDelGradiente(wPesoCapaFinal[i][j],factoresDeCambio[CANTIDADCAPAS-1][i],salidasCapaOculta2[j])
-	recalcularPesos(salidasCapaOculta2,factoresDeCambio, wPesoCapa2,salidasCapaOculta1,NEURONASCAPAOCULTA2, NEURONASCAPAOCULTA1, CANTIDADCAPAS-2)
+			wPesoCapaFinal[i][j] = (descensoDelGradiente(wPesoCapaFinal[i][j],factoresDeCambio[CANTIDADCAPAS-2][i],salidasCapaOculta2[j]))
+	recalcularPesos(salidasCapaOculta2,factoresDeCambio, wPesoCapa2,salidasCapaOculta1,NEURONASCAPAOCULTA2, NEURONASCAPAOCULTA1, CANTIDADCAPAS-3)
 	recalcularPesos(salidasCapaOculta1,factoresDeCambio, wPesoCapa1,entradasCapaInicial,NEURONASCAPAOCULTA1, NUM_PIXELES, 0)
 	
 #Reajuste de pesos
@@ -62,7 +62,7 @@ def descensoDelGradiente(pesoAnterior,factorDeCambioAnterior,entradaAnterior):
 	
 def recalcularPesos(salidasCapaActual,factorDeCambio, pesos, entradasCapaActual, cantidadNeuronasCapaActual, cantidadNeuronasCapaAnterior, capaActual):
 	for i in range(0,cantidadNeuronasCapaActual):
-		factoresDeCambio[capaActual][i] = factorDeCambioCapasOcultas(salidas,factoresDeCambio[capaActual+1]+[0],pesos[i])
+		factoresDeCambio[capaActual][i] = factorDeCambioCapasOcultas(salidasCapaActual[i],factoresDeCambio[capaActual+1],pesos[i])
 		for j in range(0,cantidadNeuronasCapaAnterior):
 			pesos[i][j] = descensoDelGradiente(pesos[i][j],factoresDeCambio[capaActual][i],entradasCapaActual[j])
 
@@ -71,11 +71,12 @@ def error(salidaDeseada,salidaFinal):
 	suma = 0
 	for i in range(0,NUM_CLASES):
 		suma += (salidaDeseada[i] - salidaFinal[i])**2
-	return math.sqrt(suma)
+	return float(math.sqrt(suma))
 	
 #Funciones auxiliares---------------------------------------------
 def sigmoide(valor):
-	return (float(1/(1+math.exp(-valor))))
+	print valor
+	return float(1/(1+math.exp(-valor)))
 
 def productoInterno(x,w):
 	producto = 0
@@ -84,7 +85,7 @@ def productoInterno(x,w):
 	return producto
 	
 def escalamiento(valor):
-	return (valor - VALORMINIMO)/(VALORMAXIMO - VALORMINIMO)
+	return (float(valor - VALORMINIMO)/(VALORMAXIMO - VALORMINIMO))
 	
 def funcionDeActivacion(entrada,pesos):
 	return (sigmoide(productoInterno(entrada,pesos)))
@@ -94,14 +95,12 @@ def derivadaSalida(salida):
 	
 def calcularSalidasDeCapa(cantidadNeuronas,salidas,entradas,pesos):
 	for i in range(0,cantidadNeuronas):
-		print len(entradas)
-		print len(pesos[i])
-		salidas[i] = float(funcionDeActivacion(entradas,pesos[i]))
+		salidas[i] = float(funcionDeActivacion(entradas,pesos[i] + [UMBRALINICIAL]))
 		
-def modificarVector(vector,inicioVector,tamanio):	
-	vectorADevolver = vector[:]
-	for i in range(inicioVector,tamanio+1):
-		vectorADevolver[i-1] = (escalamiento(int(vector[i])))
+def modificarVector(vector,tamanio):	
+	vectorADevolver = range(0,tamanio-1)
+	for i in range(1,tamanio):
+		vectorADevolver[i-1] = escalamiento(int(vector[i]))
 	return (vectorADevolver + [1])
 #---------------------------------------------------------------
 #Funcion de entrenamiento
@@ -113,29 +112,32 @@ def entrenamiento(setDeDatos):
 		salidasDeseadas[int(setDeDatos[i][0])] = 1
 		#calcularSalidasDeCapa(cantidadNeuronasCapaActual,salidasCapaActual,salidasCapaAnterior,pesosCapaActual)
 		#se calcula la salida de la capa oculta 1
-		calcularSalidasDeCapa(NEURONASCAPAOCULTA1,salidasCapaOculta1,modificarVector(fila,1,NUM_PIXELES),wPesoCapa1)
+		calcularSalidasDeCapa(NEURONASCAPAOCULTA1,salidasCapaOculta1,modificarVector(fila,len(setDeDatos[i])),wPesoCapa1)
 		#se calcula la salida de la capa oculta 2
 		calcularSalidasDeCapa(NEURONASCAPAOCULTA2,salidasCapaOculta2,salidasCapaOculta1+[1],wPesoCapa2)
 		#se calcula la salida de la capa final
 		calcularSalidasDeCapa(NUM_CLASES,salidasCapaFinal,salidasCapaOculta2+[1],wPesoCapaFinal)
 		contadorPosiciones = 0
-		if (error(salidasDeseadas,salidasCapaFinal) > CRITERIODECORTE):	
+		if (error(salidasDeseadas,salidasCapaFinal) > CRITERIODECORTE):
 			while (contadorPosiciones <= i):		
 				#recalculamos todos los pesos de la red
-				backpropagation(salidasDeseadas,salidasCapaFinal,salidasCapaOculta1,salidasCapaOculta2,wPesoCapa1,wPesoCapa2,wPesoCapaFinal,entradasCapaInicial)
+				backpropagation(salidasDeseadas,salidasCapaFinal,salidasCapaOculta1,salidasCapaOculta2,wPesoCapa1,wPesoCapa2,wPesoCapaFinal,modificarVector(fila,len(setDeDatos[i])))
 				#calculamos otras vez las salidas de todas las capas
-				fila = setDeDatos[contadorDePosiciones]
+				fila = setDeDatos[contadorPosiciones]
 				for j in range(0,NUM_CLASES):
 					salidasDeseadas.append(0)
 				salidasDeseadas[int(setDeDatos[i][0])] = 1
-				calcularSalidasDeCapa(NEURONASCAPAOCULTA1,salidasCapaOculta1,modificarVector(fila,1,NUM_PIXELES),wPesoCapa1)
+				calcularSalidasDeCapa(NEURONASCAPAOCULTA1,salidasCapaOculta1,modificarVector(fila,len(setDeDatos[contadorPosiciones])),wPesoCapa1)
 				calcularSalidasDeCapa(NEURONASCAPAOCULTA2,salidasCapaOculta2,salidasCapaOculta1+[1],wPesoCapa2)
 				calcularSalidasDeCapa(NUM_CLASES,salidasCapaFinal,salidasCapaOculta2+[1],wPesoCapaFinal)
 				if (error(salidasDeseadas,salidasCapaFinal) > CRITERIODECORTE):
 					contadorPosiciones =-1	
+				for x in salidasDeseadas[:]:
+					salidasDeseadas.remove(x)
 				contadorPosiciones +=1
-				salidasDeseadas.clear()
-				entradasCapaInicial.clear()	
+				for j in range(0,NUM_CLASES):
+					salidasDeseadas.append(0)
+				salidasDeseadas[int(setDeDatos[i][0])] = 1
 			
 	
 #Inicializacion de pesos
